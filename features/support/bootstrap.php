6e99efdadf4cfb7b42930c92432065137a178397
everzet <ever.zet@gmail.com>
2011-04-06 17:26:57 +0300

    use system-wide Behat and Mink, installed from PEAR instead of submodules

--- features/support/bootstrap.php
+++ features/support/bootstrap.php
@@ -1,4 +1,5 @@
 <?php
-require_once __DIR__.'/../../vendor/mink/autoload.php';
+
+require_once 'mink/autoload.php';
 require_once 'PHPUnit/Autoload.php';
 require_once 'PHPUnit/Framework/Assert/Functions.php';
