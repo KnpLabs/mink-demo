5634a316470bcae79ddca08b099c16c4d3f27ff6
everzet <ever.zet@gmail.com>
2011-04-06 18:01:33 +0300

    simplified wait() call. No need in direct driver call, as Session provides abstraction over it

--- features/steps/wait.php
+++ features/steps/wait.php
@@ -1,6 +1,5 @@
 <?php
 
 $steps->And('/^I wait for the suggestion box to appear$/', function($world) {
-    $world->getSession()->getDriver()->getClient()->wait(5000, "$('.suggestions-results').children().length > 0");
+    $world->getSession()->wait(5000, "$('.suggestions-results').children().length > 0");
 });
-
