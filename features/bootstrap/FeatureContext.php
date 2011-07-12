44ae580bd7d21a29a87abe82d279e45e183a5e1e
everzet <ever.zet@gmail.com>
2011-07-12 23:15:51 +0300

    updated to Behat 2.0 and Mink RC3

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -2,12 +2,12 @@
 
 use Behat\Behat\Context\ClosuredContextInterface,
     Behat\Behat\Context\BehatContext,
-    Behat\Behat\Exception\Pending;
+    Behat\Behat\Exception\PendingException;
 
 use Behat\Gherkin\Node\PyStringNode,
     Behat\Gherkin\Node\TableNode;
 
-require_once __DIR__ . '/../../mink-1.0.0RC1.phar';
+require_once __DIR__ . '/../../mink-1.0.0RC3.phar';
 
 /**
  * Features context.
@@ -17,7 +17,7 @@ class FeatureContext extends Behat\Mink\Behat\Context\MinkContext
     /**
      * @Then /^I wait for the suggestion box to appear$/
      */
-    public function waitForSuggestionBox()
+    public function iWaitForTheSuggestionBoxToAppear()
     {
         $this->getSession()->wait(5000, "$('.suggestions-results').children().length > 0");
     }
